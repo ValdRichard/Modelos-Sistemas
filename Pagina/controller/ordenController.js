e11b07183d60a4a84edc10f9c2fd390bd19d5124
Tomas Ismael Mendoza <100981517+leopoldo109@users.noreply.github.com>
2022-11-27 14:46:22 -0300

    Update ordenController.js

--- Pagina/controller/ordenController.js
+++ Pagina/controller/ordenController.js
@@ -1,7 +1,9 @@
+//Herramientas
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../db/Connection.js");
 const { ordenes } = require("../models/Orden.js");
 
+//Trae las ordenes
 module.exports = {
   order: async function (req, res) {
     let order = await ordenes.findByPk(req.params.id, {
