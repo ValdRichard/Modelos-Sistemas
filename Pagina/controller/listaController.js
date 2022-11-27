788c81c5f3ecfe14847d1b8f966d4ab72cb9a0a3
Tomas Ismael Mendoza <100981517+leopoldo109@users.noreply.github.com>
2022-11-27 14:45:17 -0300

    Update listaController.js

--- Pagina/controller/listaController.js
+++ Pagina/controller/listaController.js
@@ -1,8 +1,10 @@
+//Herramientas
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../db/Connection.js");
 
 const { usuarios } = require("../models/Usuario.js");
 
+//Rellena la tabla con los usuarios
 async function getAll(req, res, next) {
   let Usuarios = await usuarios.findAll();
 
